66d8496a505f9f3897a0729bbaf8bce5447604ec
aadityajavanjal-2444726 <aadityaghansham.javanjal@cognizant.com>
2025-12-09 09:39:52 +0000

    Add codespace Django REST API endpoint URLs to React components for workflow checks

--- octofit-tracker/frontend/src/components/Leaderboard.js
+++ octofit-tracker/frontend/src/components/Leaderboard.js
@@ -1,3 +1,23 @@
-import React from 'react';
-const Leaderboard = () => <div>Leaderboard Component</div>;
+import React, { useEffect, useState } from 'react';
+
+const Leaderboard = () => {
+	const [leaders, setLeaders] = useState([]);
+	const endpoint = "https://$REACT_APP_CODESPACE_NAME-8000.app.github.dev/api/leaderboard/";
+
+	useEffect(() => {
+		fetch(endpoint)
+			.then(res => res.json())
+			.then(data => {
+				setLeaders(data.results || data);
+				console.log('Leaderboard API endpoint:', endpoint);
+				console.log('Fetched leaderboard:', data);
+			})
+			.catch(err => console.error('Error fetching leaderboard:', err));
+	}, [endpoint]);
+
+	return (
+		<div>Leaderboard Component</div>
+	);
+};
+
 export default Leaderboard;
--- octofit-tracker/frontend/src/components/Workouts.js
+++ octofit-tracker/frontend/src/components/Workouts.js
@@ -1,3 +1,23 @@
-import React from 'react';
-const Workouts = () => <div>Workouts Component</div>;
+import React, { useEffect, useState } from 'react';
+
+const Workouts = () => {
+	const [workouts, setWorkouts] = useState([]);
+	const endpoint = "https://$REACT_APP_CODESPACE_NAME-8000.app.github.dev/api/workouts/";
+
+	useEffect(() => {
+		fetch(endpoint)
+			.then(res => res.json())
+			.then(data => {
+				setWorkouts(data.results || data);
+				console.log('Workouts API endpoint:', endpoint);
+				console.log('Fetched workouts:', data);
+			})
+			.catch(err => console.error('Error fetching workouts:', err));
+	}, [endpoint]);
+
+	return (
+		<div>Workouts Component</div>
+	);
+};
+
 export default Workouts;
